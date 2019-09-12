3caa874e65f1ce5cf685a54d4a583954bf5dd1f2
bit <bit@gameboyzero.es>
2019-09-12 14:27:14 +0200

    insertados cambios en el instalador para que no haya que cambiar nada

--- install.sh
+++ install.sh
@@ -9,11 +9,15 @@ rm -Rf addon
 cat alfa/core/httptools.py | sed -e "s/HTTPTOOLS_DEFAULT_DOWNLOAD_TIMEOUT = config.get_setting('httptools_timeout', default=15)/HTTPTOOLS_DEFAULT_DOWNLOAD_TIMEOUT = 15/" > alfa/core/httptools.py.new
 mv alfa/core/httptools.py.new alfa/core/httptools.py
 
-#transmission torrent
-apt-get install transmission-cli -y
-apt-get install sox -y
-apt-get install python-cryptography -y
-apt-get install ffmpeg -y
-apt-get install python-cryptography -y
+#transmission
+sudo apt-get install transmission-cli sox python-cryptography ffmpeg -y
+
+#tg for raspberry or debian
+#sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev libjansson-dev libpython-dev libpython3-dev libgcrypt-dev zlib1g-dev lua-lgi make -y
+sudo apt-get install libreadline-dev libconfig-dev libssl1.0-dev lua5.2 liblua5.2-dev libevent-dev libjansson-dev libpython-dev libpython3-dev libgcrypt-dev zlib1g-dev lua-lgi make -y
+git clone --recursive https://github.com/kenorb-contrib/tg /home/pi/tg
+cd /home/pi/tg
+./configure
+make -j4
 
 echo "done!"

21ab9252fe55785af19d927210a1d7ad58fc97ac
Bit <bitstuffing@gmail.com>
2019-11-17 18:02:04 +0100

    added script to update engine and flipax modular provider (not finished yet)

--- bot/providers/flipax.py
+++ bot/providers/flipax.py
@@ -10,26 +10,70 @@ class Flipax():
     FLIPAX_PASSWORD = Config.getConfig()["FLIPAX_PASSWORD"]
     FLIPAX_USERNAME = Config.getConfig()["FLIPAX_USERNAME"]
 
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:54.0) Gecko/20100101 Firefox/58.0','Connection': 'keep-alive'}
+
     session = ''
 
     @staticmethod
-    def getSection(section='', content=''):
-        if Flipax.session == '' and content != '':
-            Flipax.session = requests.get(Flipax.MAIN).cookies
-            logger.debug("Cookie %s"%str(Flipax.session))
-            requests.post(Flipax.MAIN+"/login",data={
-                'username':Flipax.FLIPAX_USERNAME,
-                'password':Flipax.FLIPAX_PASSWORD,
-                'autologin':'on',
-                'redirect':'',
-                'query':'',
-                'login':'Conectarse',
-            })
+    def login():
+        Flipax.session = requests.get(Flipax.MAIN).cookies
+        logger.debug("Cookie %s"%str(Flipax.session))
+        response = requests.post(Flipax.MAIN+"/login",data={
+            'username':Flipax.FLIPAX_USERNAME,
+            'password':Flipax.FLIPAX_PASSWORD,
+            'autologin':'on',
+            'redirect':'',
+            'query':'',
+            'login':'Conectarse',
+        })
+        logger.debug(str(response))
 
-        session = Flipax.session
-        logger.debug("Using session %s "%str(session))
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:54.0) Gecko/20100101 Firefox/58.0','Connection': 'keep-alive'}
-        html = requests.get(Flipax.MAIN, headers=headers, cookies=session, verify=True).text
+    @staticmethod
+    def extractSection(link):
+        logger.debug("found link!")
+        html = requests.get(link, headers=Flipax.headers, cookies=Flipax.session, verify=True).text
+        elements = []
+        found = False
+        for block in html.split('<ul class="topiclist topics bg_none">'):
+            i=0
+            for field in block.split('<li class="row '):
+                if i>0:
+                    element = {}
+                    title = Decoder.extract('class="topictitle"','</a>',field)
+                    title = title[title.find(">")+1:]
+                    link = Flipax.MAIN+Decoder.extract(' href="','"',field)
+                    element["title"] = title
+                    element["link"] = link
+                    logger.debug(title+'.-.'+link)
+                    if len(title)>0 and len(content)==0:
+                        elements.append(element)
+                    elif content in element["title"]:
+                        logger.debug("found link: %s"%content)
+                        link = element["link"]
+                        logger.debug("link is :%s"%link)
+                        found = True
+                        break
+                    else:
+                        logger.debug("not found: %s|%s"%(content,element["title"]))
+                i+=1
+        return link
+
+    @staticmethod
+    def extractLinks(link):
+        html = requests.get(link, headers=Flipax.headers,cookies=Flipax.session, verify=True).text
+        section = Decoder.extract('<div class="postbody"><div class="content"><div><div align="center">','</div></div></div>',html)
+        logger.debug("html is: %s"%section)
+        for link in section.split('target="_blank" rel="nofollow">'):
+            link = link[:link.find('<')]
+            element = {}
+            element["title"] = link
+            element["link"] = link
+            elements.append(element)
+
+    @staticmethod
+    def extractSections():
+        logger.debug("Using session %s "%str(Flipax.sessions))
+        html = requests.get(Flipax.MAIN, headers=headers, cookies=Flipax.session, verify=True).text
         elements = []
         for block in html.split('<ul class="topiclist forums">'):
             i=0
@@ -44,6 +88,17 @@ class Flipax():
                     if len(title)>0:
                         elements.append(element)
                 i+=1
+
+        return elements
+
+    @staticmethod
+    def getSection(section='', content=''):
+        elements = []
+
+        if Flipax.session == '' and content != '':
+            Flipax.login()
+
+        elements = Flipax.extractSections()
         if len(section)>0:
             link = ''
             for element in elements:
@@ -52,39 +107,7 @@ class Flipax():
                     link = element["link"]
                     break
             if len(link)>0:
-                logger.debug("found link!")
-                html = requests.get(link, headers=headers, cookies=session, verify=True).text
-                elements = []
-                found = False
-                for block in html.split('<ul class="topiclist topics bg_none">'):
-                    i=0
-                    for field in block.split('<li class="row '):
-                        if i>0:
-                            element = {}
-                            title = Decoder.extract('class="topictitle"','</a>',field)
-                            title = title[title.find(">")+1:]
-                            link = Flipax.MAIN+Decoder.extract(' href="','"',field)
-                            element["title"] = title
-                            element["link"] = link
-                            logger.debug(title+'.-.'+link)
-                            if len(title)>0 and len(content)==0:
-                                elements.append(element)
-                            elif content in element["title"]:
-                                logger.debug("found link: %s"%content)
-                                link = element["link"]
-                                found = True
-                                break
-                            else:
-                                logger.debug("not found: %s|%s"%(content,element["title"]))
-                        i+=1
-                if found:
-                    html = requests.get(link, headers=headers,cookies=session, verify=True).text
-                    section = Decoder.extract('<div class="postbody"><div class="content"><div><div align="center">','</div></div></div>',html)
-                    logger.debug("html is: %s"%section)
-                    for link in section.split('target="_blank" rel="nofollow">'):
-                        link = link[:link.find('<')]
-                        element = {}
-                        element["title"] = link
-                        element["link"] = link
-                        elements.append(element)
+                link2 = Flipax.extractSection(link)
+                if link2 != link:
+                    elements = Flipax.extractLinks(link2)
         return elements
---
+++ engine.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#update/install alfa engine
+git clone https://github.com/alfa-addon/addon
+mv addon/plugin.video.alfa alfa
+rm -Rf addon
+
